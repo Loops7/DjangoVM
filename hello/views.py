c283cee40832ffd6efb28a4a5231a8173eb3d534
Joey <jlupohockey@gmail.com>
2015-11-22 22:13:08 -0600

    Added scikit learn

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#import numpy as np
+import numpy as np
 #import scipy
-#from sklearn.datasets import load_digits
-# Create your views here.
+from sklearn.datasets import load_digits
+
 def hey(request):
-    #digits = load_digits()
-    #X,y = digits.data, digits.target 
-    #y = y[50:60]
-    #return render(request, "hello/home.html", {'digits':y})
-    return HttpResponse("A")
+    digits = load_digits()
+    X,y = digits.data, digits.target 
+    y = y[50:60]
+    return render(request, "hello/home.html", {'digits':y})
