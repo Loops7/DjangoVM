c70650079c124a3c6714ab607f8264d3cf3daac5
Joey <jlupohockey@gmail.com>
2015-11-30 20:30:05 -0600

    Removed np,scipy,sklearn from view

--- hello/views.py
+++ hello/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import numpy as np
-import scipy
-from sklearn.datasets import load_digits
+#import numpy as np
+#import scipy
+#from sklearn.datasets import load_digits
 
 def hey(request):
-    digits = load_digits()
-    X,y = digits.data, digits.target 
-    y = y[20:60:4]
-    return render(request, "hello/home.html", {'digits':y})
+    #digits = load_digits()
+    #X,y = digits.data, digits.target 
+    #y = y[20:60:4]
+    return render(request, "hello/home.html", {'digits':range(10)})
